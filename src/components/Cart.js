0152baeac6eb9280ec2141de0c3dbddfed8a365e
Fredrik Holm <karlfredrikholm@gmail.com>
2022-11-02 12:02:00 +0100

    completed codealong

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { CartItem } from './CartItem'
 
 export const Cart = () => {
   // TODO - fetch products from the cart store
-  const products = []
+  const products = useSelector((state) => state.cart.items)
 
   // TODO - calculate total from the sum of all products in the cart
-  const totalPrice = 0
+  const totalPrice = useSelector((state) => (
+    state.cart.items.reduce((total, item) => (item.price + (item.price * item.quantity)), 0)
+  ))
 
   return (
     <div className="cart">
